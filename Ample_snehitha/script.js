fbe16a0ff07a335ef855ca09793144ddbd0379ce
AnushaReddy1207 <anushareddysiripireddy466@gmail.com>
2024-09-17 08:49:02 +0530

    Add files via upload

---
+++ Ample_snehitha/script.js
@@ -0,0 +1,54 @@
+document.getElementById("disBut").addEventListener("click", function() {
+    const image = document.getElementById("image");
+    if (image.style.display === "none") {
+        image.style.display = "block";
+       
+    } else {
+        image.style.display = "none";
+        }
+});
+document.getElementById("disBut1").addEventListener("click", function() {
+    const video = document.getElementById("video1");
+    if (video.style.display === "none") {
+        video.style.display = "block";
+       
+    } else {
+        video.style.display = "none";
+        }
+});
+document.getElementById("disBut2").addEventListener("click", function() {
+    const image = document.getElementById("image2");
+    if (image.style.display === "none") {
+        image.style.display = "block";
+       
+    } else {
+        image.style.display = "none";
+        }
+});
+document.getElementById("disBut3").addEventListener("click", function() {
+    const image = document.getElementById("image3");
+    if (image.style.display === "none") {
+        image.style.display = "block";
+       
+    } else {
+        image.style.display = "none";
+        }
+});
+document.getElementById("disBut4").addEventListener("click", function() {
+    const image = document.getElementById("image4");
+    if (image.style.display === "none") {
+        image.style.display = "block";
+       
+    } else {
+        image.style.display = "none";
+        }
+});
+document.getElementById("disBut5").addEventListener("click", function() {
+    const video = document.getElementById("video5");
+    if (video.style.display === "none") {
+        video.style.display = "block";
+       
+    } else {
+        video.style.display = "none";
+        }
+});
